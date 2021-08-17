f14a3d8e18b62f3bf38b1dbbe610a19f9e04c0e2
Ash <ext0l@riseup.net>
2021-08-17 11:31:05 -0700

    Add mod-dh support (#9)
    Setting evil-colemak-basics-layout-mod to 'mod-dh enables this.
    
    This just swaps h and m.

--- evil-colemak-basics.el
+++ evil-colemak-basics.el
@@ -35,6 +35,16 @@
   :prefix "evil-colemak-basics-"
   :group 'evil)
 
+(defcustom evil-colemak-basics-layout-mod nil
+  "Which Colemak layout mod to use.
+
+Colemak Mod-DH, also known as Colemak Mod-DHm, has m where the h
+key is on qwerty. This means we need to swap the h and m
+bindings. No other changes are necessary."
+  :group 'evil-colemak-basics
+  :type '(choice (const :tag "default" nil)
+                 (const :tag "mod-dh" mod-dh)))
+
 (defcustom evil-colemak-basics-rotate-t-f-j t
   "Whether to keep find-char and end of word jumps at their qwerty position.
 
@@ -104,10 +114,10 @@ rotated; see evil-colemak-basics-rotate-t-f-j."
       (cond
        ((eq evil-colemak-basics-char-jump-commands nil)
         (evil-define-key '(motion normal visual) keymap
-            "t" 'evil-find-char
-            "T" 'evil-find-char-backward
-            "j" 'evil-find-char-to
-            "J" 'evil-find-char-to-backward))
+          "t" 'evil-find-char
+          "T" 'evil-find-char-backward
+          "j" 'evil-find-char-to
+          "J" 'evil-find-char-to-backward))
        ((eq evil-colemak-basics-char-jump-commands 'evil-snipe)
         ;; XXX https://github.com/hlissner/evil-snipe/issues/46
         (evil-snipe-def 1 inclusive "t" "T")
@@ -118,6 +128,14 @@ rotated; see evil-colemak-basics-rotate-t-f-j."
           "j" 'evil-snipe-j
           "J" 'evil-snipe-J))
        (t (user-error "Invalid evil-colemak-basics-char-jump-commands configuration"))))
+    (when (eq evil-colemak-basics-layout-mod 'mod-dh)
+      (evil-define-key '(motion normal visual) keymap
+        "m" 'evil-backward-char)
+      (evil-define-key 'normal keymap
+	"h" 'evil-set-marker)
+      (evil-define-key 'motion keymap
+	"H" 'evil-window-top
+	"M" 'evil-window-middle))
     keymap))
 
 (defvar evil-colemak-basics-keymap
