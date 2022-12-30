ea519b962f051cabced8aadaf6ed0134b861225c
mk3z <52108954+mk3z@users.noreply.github.com>
2022-12-30 16:43:10 +0200

    Add support for evil-respect-visual-line-mode (#19)

--- evil-colemak-basics.el
+++ evil-colemak-basics.el
@@ -146,6 +146,17 @@ rotated; see evil-colemak-basics-rotate-t-f-j."
         "m" 'evil-backward-char)
       (evil-define-key '(normal visual) keymap
         "h" 'evil-set-marker))
+    (when evil-respect-visual-line-mode
+      (evil-define-key '(motion normal visual) keymap
+        "n" 'evil-next-visual-line
+        "gn" 'evil-next-line
+        "e" 'evil-previous-visual-line
+        "ge" 'evil-previous-line
+        "0" 'evil-beginning-of-visual-line
+        "g0" 'evil-beginning-of-line
+        "$" 'evil-end-of-visual-line
+        "g$" 'evil-end-of-line
+        "V" 'evil-visual-screen-line))
     keymap))
 
 (defvar evil-colemak-basics-keymap
